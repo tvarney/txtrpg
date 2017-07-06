d790682696ae6e2f82014dca45f3a599bab6030d
tvarney <troy.a.varney@gmail.com>
2017-07-06 12:45:20 -0500

    Fixed base package items.py missing the parry value for all weapons.

--- data/packages/base/items.py
+++ data/packages/base/items.py
@@ -3,14 +3,14 @@ from rpg.data import item
 
 
 weapon_bronze_short_sword = item.Weapon(
-    "weapon.short_sword_bronze", item.WeaponType.ShortSword, "Bronze Short Sword", 10, 1,
+    "weapon.short_sword_bronze", item.WeaponType.ShortSword, "Bronze Short Sword", 10, 1, 0,
     [item.Attack("Slash", 10, 10), item.Attack("Stab", 5, 10)], 5
 )
 weapon_bronze_long_sword = item.Weapon(
-    "weapon.long_sword_bronze", item.WeaponType.LongSword, "Bronze Long Sword", 10, 1,
+    "weapon.long_sword_bronze", item.WeaponType.LongSword, "Bronze Long Sword", 10, 1, 0,
     [item.Attack("Slash", 5, 10), item.Attack("Stab", 10, 10)], 5
 )
 weapon_bronze_dagger = item.Weapon(
-    "weapon.dagger_bronze", item.WeaponType.Dagger, "Bronze Dagger", 10, 1,
+    "weapon.dagger_bronze", item.WeaponType.Dagger, "Bronze Dagger", 10, 1, 0,
     [item.Attack("Slash", 5, 10), item.Attack("Stab", 5, 10)], 5
 )
